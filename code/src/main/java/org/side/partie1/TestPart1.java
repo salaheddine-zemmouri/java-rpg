aef102bb1e247c625b1c6998fd7cc21708c418a2
ibrahim-mestadi <ibrahim_mestadi@um5.ac.ma>
2022-11-10 01:41:58 +0100

    partie 1

---
+++ code/src/main/java/org/side/partie1/TestPart1.java
@@ -0,0 +1,9 @@
+package org.side.partie1;
+
+import java.rmi.server.UID;
+
+public class TestPart1 {
+    public static void main(String[] args) {
+        Monster monster = MonsterFactory.createMonster("orc") ;
+    }
+}
